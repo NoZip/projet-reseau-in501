f74816c996fc75c2ea2f2a8c41513cabe73eab99
Thomas Pizon <pizon.thomas@gmail.com>
2012-11-21 17:24:10 +0100

    COrrections des accesseurs de Pod

--- src/Pod.java
+++ src/Pod.java
@@ -9,43 +9,44 @@
 	import java.nio.*;
 	import java.nio.channels.*;
 	import org.json.JSONObject;
-	
+
 	public class Pod {
-		
+
 		private Map<String, Service> services;
 		private Map<String, String[]> friends;
 		private List<Message> messages;
-		
+
 		public Pod(Map<String, Service> services,String username){
 			this.services = new Hashtable<String, Service>();
 			this.friends = new Hashtable<String, String[]>();
 			this.messages = new Vector<Message>();
 		}
-		
+
 		public void listen(int port) {
 			try {
 				ServerSocket serverSocket = new ServerSocket(port); //creation du serveur
-				
+
 				// Boucle d'écoute
 				while(true){
 					Socket socket = serverSocket.accept();
-					
+
 					// On redirige le socket qui s'est connecté vers un thread
 					ConnectionThread handler = new ConnectionThread(this, socket);
 					handler.run();
 				}
-			}catch(Exception e){
+			} catch(Exception e){
 				;//tant pis
 			}
 		}
-		
+
 		/**
 		* Envoie une commande à un autre Pod
 		* @param url
 		* @param command
 		* @param arguments
+		* @return La réponse de l'autre Pod.
 		*/
-		public void sendCommand(String url, int port, String command, JSONObject arguments) {
+		public JSONObject sendCommand(String url, int port, String command, JSONObject arguments) {
 			try {
 				InetAddress addr = java.net.InetAddress.getByName(url);//on recupere l'adresse correspondant a l'url
 				Socket sock = new Socket(addr,port); //on cree la socket d'ecriture
@@ -54,64 +55,84 @@
 				String tmp = new String(tmpBuf);
 				os.write(tmp.getBytes()); //on l'envoie sous forme de bytes
 				sock.close();
-			}catch(Exception e){
+			} catch(Exception e){
 				;//tant pis
 			}
 		}
-		
+
 		/**
 		* Récupère un service grace à sa commande.
 		* @param command
+		* @return le service demandé.
 		*/
-		public void getService(String command) {
-			;//return services.get(command);
+		public Service getService(String command) {
+			return services.get(command);
 		}
-		
+
 		/**
 		* Ajoute un service au pod.
 		* @param command
 		* @param service
 		*/
-		public synchronized void addService(String command, Service service) {
-			services.put(command,service);
+		public void addService(String command, Service service) {
+			synchronized(services) {
+				services.put(command, service);
+			}
 		}
-		
+
 		/**
 		* Retire un service du pod.
 		* @param command
 		*/
-		public synchronized void removeService(String command) {
-			services.remove(command);
+		public void removeService(String command) {
+			synchronized(services) {
+				services.remove(command);
+			}
 		}
-		
+
+		/**
+		 * Récupère l'url d'un ami.
+		 * @param name le nom de l'ami.
+		 */
+		public String getFriendUrl(String name) {
+			return friends.get(name);
+		}
+
 		/**
 		* Ajoute un ami.
-		* @param url
+		* @param name Le nom de l'ami.
+		* @param url Une url du type "192.168.1.42:5555
 		*/
-		public synchronized void addFriend(String url,int port) {
-			;
+		public void addFriend(String name, String url) {
+			synchronized(friends) {
+				friends.put(name, url);
+			}
 		}
-		
+
 		/**
 		* Supprime un ami
-		* @param url
+		* @param name
 		*/
-		public synchronized void removeFriend(String name) {
-			friends.remove(name);
+		public void removeFriend(String name) {
+			synchronized(services) {
+				friends.remove(name);
+			}
 		}
-		
+
 		/**
 		* Récupère les messages du pod.
 		*/
-		public synchronized void getMessages() {
-			;
+		public synchronized List<Message> getMessages() {
+			return messages.clone();
 		}
-		
+
 		/**
 		* Ajoute un message au pod.
-		* @param message
+		* @param message le message à ajouter.
 		*/
-		public synchronized void addMessage(Message message) {
-			;
+		public void addMessage(Message message) {
+			synchronized(messages) {
+				messages.add(message);
+			}
 		}
 	}
