350f1034cd01fe8b0ace4deb12c1b147d8968f49
krumdine <turonjeremy@gmail.com>
2012-12-22 23:47:51 +0100

    Amelioration de la gestion de pendingFriends : on renvoie les invitations à intervalle régulier.

--- src/Start.java
+++ src/Start.java
@@ -2,14 +2,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.InetAddress;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Iterator;
 
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Start {
 
 	public static void main(String[] args){
-		Pod pod = new Pod(args[0],Integer.parseInt(args[1]));
+		final Pod pod = new Pod(args[0],Integer.parseInt(args[1]));
 		pod.addService("MSG", new MessageService(pod));
 		pod.addService("ADD", new AddService(pod));
 		pod.addService("USER", new UserService(pod));
@@ -48,6 +53,29 @@ public class Start {
 				e.printStackTrace();
 			}
 		}
+		
+		Timer timer = new Timer();
+        timer.schedule (new TimerTask() {
+            public void run()
+            {
+            	Iterator<PodLocation> it = pod.getPendingFriends().iterator();
+            	while(it.hasNext()){
+            		PodLocation pl = it.next();
+            		try {
+            			JSONObject json = new JSONObject() ;
+        				json.put("profile", pod.owner.toJSON());
+        				json.put("listening_port", pod.listeningPort);
+            			pod.sendCommand(pl.getAddress(),
+        						pl.getPort(),
+        						"ADD",
+        						json);
+        			}
+        			catch (JSONException e) {
+        				e.printStackTrace();
+        			}
+            	}
+            }
+        }, 60000); //toutes les minutes on réessaye d'envoyer une invvitation aux personnes n'ayant pas répondu.
 		
 	}
 	
